6e412788c21f5e71f675708c5af09f167d6cee1a
Tom <tcollins23106@gmail.com>
2025-09-08 21:11:25 +0100

    Update custom_terminal.py

--- custom_terminal.py
+++ custom_terminal.py
@@ -15,20 +15,28 @@ def delayed_print(text, delay=0.5):
 
 # Command functions
 
-# ---------
+# --------- HELP ---------
 
 def help():
 
-    print("For more information on a specific command, just figure it out because I'm not adding all that.")
-    print("")
-    print("HELP                Displays the list of commands, duh") 
-    print("EXIT                Exits the terminal")
-    print("")
-    print("HELLO               Prints 'hello world'")
-    print("PWORDGEN            Starts a password generator")
-    print("")
+    lines=[
+    "For more information on a specific command, just figure it out because I'm not adding all that.",
+    "",
+    "HELP                Displays the list of commands, duh",
+    "EXIT                Exits the terminal",
+    "",
+    "INFO   [Invalid]    Relays terminal info",
+    "GOTOP  [Invalid]    Displays GOTOP",
+    "NEOFETCH            Displays NEOFETCH",
+    "",
+    "HELLO               Prints 'hello world'",
+    "PWORDGEN            Starts a password generator",
+    ""
+    ]
 
-# ---------
+    print("\n" + "\n".join(lines) + "\n")
+
+# --------- EXIT ---------
 
 def exit_terminal():
     
@@ -40,61 +48,93 @@ def exit_terminal():
         print("Returning to terminal.")
         delayed_print("")
 
-# ---------
+# --------- HELLO ---------
 
 def hello():
 
     print("Hello World!")
     delayed_print("")
 
-# ---------
+# --------- PASSWORD ---------
 
 def generate_password():
-    while True:
-        delayed_print("PASSWORD GENERATOR", 0.5)
-        delayed_print("Usage: 'generate password [length]", 0.5)
-        delayed_print("-----------------")
-        generate = input("> ").strip().lower()
-        parts = generate.split()
+    delayed_print("PASSWORD GENERATOR", 0.5)
+    delayed_print("Usage: 'generate password [length]", 0.5)
+    delayed_print("-----------------")
+    generate = input("> ").strip().lower()
+    parts = generate.split()
 
-        if len(parts) == 3 and parts[0] == "generate" and parts[1] == "password":
-            if parts[2].isdigit():
-                length = int(parts[2])
-                if length < 5:
-                    delayed_print("Minimum length of 5 characters.")
-                else:
-                    # Create pool of characters to choose from
-                    characters = string.ascii_letters + string.digits
-
-                    # Generate password
-                    password_random = random.choices(characters, k=length)
-                    password = ''.join(password_random)
-
-                    delayed_print("-----------------")
-                    if length > 20:
-                        delayed_print("Are you really going to remember this?")
-                        delayed_print("\n")
-                    delayed_print("Your password:")
-                    delayed_print(password)
-                    print("")
-                    break
+    if len(parts) == 3 and parts[0] == "generate" and parts[1] == "password":
+        if parts[2].isdigit():
+            length = int(parts[2])
+            if length < 5:
+                delayed_print("Minimum length of 5 characters.")
             else:
-                delayed_print("Not a valid password length.")
+                # Create pool of characters to choose from
+                characters = string.ascii_letters + string.digits
+
+                # Generate password
+                password_random = random.choices(characters, k=length)
+                password = ''.join(password_random)
+
+                delayed_print("-----------------")
+                if length > 20:
+                    delayed_print("Are you really going to remember this?")
+                    delayed_print("\n")
+                delayed_print("Your password:")
+                delayed_print(password)
+                print("")
         else:
+            delayed_print("Not a valid password length.")
+    else:
             delayed_print("Unrecognised command.")
 
+# --------- INFO ---------
 
+def info():
+    print("")
+
+# --------- GOTOP ---------
+
+def gotop():
+    print("")
+
+# --------- NEOFETCH ---------
+
+def neofetch():
+
+    lines = [
+    "JohnSilentHill@DESKTOP-1",
+    "-------------------",
+    "Uptime: 21 hours 17 minutes",
+    "OS: JohnOS [64-bit]",
+    "Packages: 5 [scoop]",
+    "-------------------",
+    "Motherboard: No motherboard found",
+    "Memory: No memory found",
+    "CPU: No CPU found",
+    "GPU: No GPU found",
+    "Disk (C:): 0.01 TiB / 1.8 TiB"
+    ]
+
+    time.sleep(1)
+    print("\n" + "\n".join(lines) + "\n")
+    
 # Dictionary
 commands = {
     "help": help,
     "exit": exit_terminal,
     "hello": hello,
+    "info": info,
+    "gotop": gotop,
+    "neofetch": neofetch,
     "pwordgen": generate_password
 }
 
 def repeaterror():
     print("Bro please just type 'help' already.")
     print("")
+    error_count = 0
 
 error_count = 0
 
@@ -115,3 +155,4 @@ while True:
         error_count += 1
         if error_count >= 10:
             repeaterror()
+
